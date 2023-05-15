466d23cf1d23cf46f3468061d946c6c022f575a7
Jay <63109802+bjw1622@users.noreply.github.com>
2023-05-15 19:06:30 +0900

    [Bronze V] Title: ??!, Time: 276 ms, Memory: 17724 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String id = scanner.next();
+        System.out.println(id + "??!");
+    }
+}
