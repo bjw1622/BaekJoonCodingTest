81dcd9623728827d083f1128d185de40f51c11b6
Jay <63109802+bjw1622@users.noreply.github.com>
2023-06-13 15:24:32 +0900

    [Silver I] Title: 그림, Time: 376 ms, Memory: 51916 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.java
+++ 백준/Silver/1926. 그림/그림.java
@@ -9,78 +9,68 @@
 //        그래프 전체 지도 : int[][]
 //        방문 여부 : boolean[][]
 //        QUEUE : BFS
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 public class Main {
-    static int n, m;
+    static int n,m;
     static int[][] graph;
     static boolean[][] visited;
-    static int count = 0;
-    static int[][] pos = {{0, -1}, {0, 1}, {1, 0}, {-1, 0}};
-
-
-    static void bfs(int x, int y) {
-        visited[x][y] = true;
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{x, y});
-
-        while (!queue.isEmpty()) {
-            int[] arr = queue.poll();
-            int nowX = arr[0];
-            int nowY = arr[1];
-
-            for (int i = 0; i < pos.length; i++) {
-                int nx = nowX + pos[i][0];
-                int ny = nowY + pos[i][1];
-
-                if (nx >= 0 && nx < n && ny >= 0 && ny < m && graph[nx][ny] == 1 && !visited[nx][ny]) {
-                    count++;
-                    visited[nx][ny] = true;
-                    queue.add(new int[]{nx, ny});
-                }
+    static int[] dx = {0,1,0,-1};
+    static int[] dy = {1,0,-1,0};
+    static int check =0;
+    static int maxV = 0;
+    public static void bfs(int x, int y){
+        visited[x][y] =  true;
+        for(int i =0; i<dx.length;i++){
+            int nx = x+dx[i];
+            int ny = y + dy[i];
+            if(nx >= 0 && nx < n && ny>=0 && ny<m && graph[nx][ny] == 1 && visited[nx][ny] == false ){
+                check += 1;
+                bfs(nx,ny);
             }
         }
-
     }
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        n = sc.nextInt();
-        m = sc.nextInt();
-        boolean zero = true;
-        List<Integer> answer = new ArrayList<>();
-
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        StringTokenizer st = new StringTokenizer(str);
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
         graph = new int[n][m];
         visited = new boolean[n][m];
-
+        for(int i = 0; i < n; ++i) {
+            String strr = br.readLine();
+            st = new StringTokenizer(strr); // length 5
+            for(int j = 0; j < m; ++j) {
+                graph[i][j] = Integer.parseInt(st.nextToken()); // substring이랑 차이??
+            }
+        }
+        List<Integer> answer = new ArrayList<>();
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                graph[i][j] = sc.nextInt();
-                if (graph[i][j] == 1) {
-                    zero = false;
+            for (int j = 0; j < m; j++){
+                check = 1;
+                if(graph[i][j] == 1 && visited[i][j] == false){
+                    bfs(i,j);
+                    answer.add(check);
+                    if (check > maxV){
+                        maxV = check;
+                    }
                 }
             }
         }
-
-        if (zero == true) {
-            System.out.println("0");
-            System.out.println("0");
-            return;
-        }
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                count = 0;
-                if (graph[i][j] == 1 && !visited[i][j]) {
-                    count++;
-                    bfs(i, j);
-                    answer.add(count);
-                }
-            }
-        }
-        Collections.sort(answer);
         System.out.println(answer.size());
-        System.out.println(answer.get(answer.size() - 1));
+        System.out.println(maxV);
+
+    }
+
+    public void printArr(){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++){
+                System.out.print(graph[i][j]+" ");
+            }
+            System.out.println();
+        }
     }
 }
-
