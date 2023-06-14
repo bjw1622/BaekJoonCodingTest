c207e00b35a658a0d7482e837abffb28d227af44
Jay <63109802+bjw1622@users.noreply.github.com>
2023-06-14 23:07:21 +0900

    [Silver IV] Title: 바닥 장식, Time: 128 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Silver/1388. 바닥 장식/바닥 장식.java
+++ 백준/Silver/1388. 바닥 장식/바닥 장식.java
@@ -3,18 +3,24 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 public class Main {
     static char[][] arr;
+    static int n;
+    static int m;
     public static void main(String[] args)throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
         arr = new char[n][m];
+
         for (int i = 0; i < n; i++) {
             String row = br.readLine();
             for (int j = 0; j < m; j++) {
                 arr[i][j] = row.charAt(j);
             }
         }
+
         int count = 0;
         for (int i = 0; i < n; i++) {
             int tmp = 0;
@@ -26,6 +32,7 @@ public class Main {
                 }
             }
         }
+
         for (int j = 0; j < m; j++) {
             int tmp = 0;
             for (int i = 0; i < n; i++) {
@@ -38,5 +45,4 @@ public class Main {
         }
         System.out.println(count);
     }
-
 }
